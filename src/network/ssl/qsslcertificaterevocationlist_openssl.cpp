267b835a283b8e582fa2e228f86d9e5d1fbdebe4
Martin Banky <Martin.Banky@gmail.com>
2017-04-06 23:40:42 -0700

    Code formatting fix
    Change-Id: I7c65b8be6144ea50c030534335475bbf9b8e3bbd
    Signed-off-by: Martin Banky <Martin.Banky@gmail.com>

--- src/network/ssl/qsslcertificaterevocationlist_openssl.cpp
+++ src/network/ssl/qsslcertificaterevocationlist_openssl.cpp
@@ -255,13 +255,13 @@ QByteArray QSslCertificateRevocationListPrivate::crlNumber()
         ASN1_INTEGER *crlAsn1Number
                 = reinterpret_cast<ASN1_INTEGER *>(q_X509_CRL_get_ext_d2i(x509Crl, NID_crl_number, 0, 0));
 
-        if(crlAsn1Number){
+        if (crlAsn1Number) {
             BIGNUM *crlBigNumber = q_ASN1_INTEGER_to_BN(crlAsn1Number, 0);
             q_ASN1_STRING_free(crlAsn1Number);
 
             m_crlNumber = q_BN_bn2dec(crlBigNumber);
             q_BN_free(crlBigNumber);
-        }else{
+        } else {
             m_crlNumber = "<None>";
         }
     }
